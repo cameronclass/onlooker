df2295bac29f507c57d16eacefff3c80f49a1355
cameronclass <kamrankutumov@gmail.com>
2022-12-02 17:45:50 +0300

    initial commit

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,2 +1,13 @@
-
-
+let newSlider = new Swiper(".twenty-five__slider_block", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".twenty-five .swiper-button-next",
+    prevEl: ".twenty-five .swiper-button-prev",
+  },
+  pagination: {
+    el: ".twenty-five .swiper-pagination",
+    clickable: true,
+  },
+});
